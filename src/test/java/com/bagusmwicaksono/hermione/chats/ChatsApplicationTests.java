20cb3504dddb63581c97ace04d3d88c9374523d8
Bagus Mahendra W <bagusmwicaksono@gmail.com>
2024-04-21 15:40:43 +0000

    first commit

---
+++ src/test/java/com/bagusmwicaksono/hermione/chats/ChatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagusmwicaksono.hermione.chats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
